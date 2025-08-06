3ad00edeeb318c517c1c124876857030d3683fa3
Kunal Kumar Jha <kunalkumarj99@gmail.com>
2025-08-06 18:17:12 +0530

    Create variables.tf

---
+++ Input variables/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "EC2 Key Pair name"
+  default     = "my-key"
+}
